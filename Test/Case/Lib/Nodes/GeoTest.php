74ca9e599700d28229c52c0a7120561f5e70a1f3
AD7six <andydawson76@gmail.com>
2012-12-18 20:52:59 +0100

    add a test for getBoundary

--- Test/Case/Lib/Nodes/GeoTest.php
+++ Test/Case/Lib/Nodes/GeoTest.php
@@ -1,5 +1,5 @@
 <?php
-App::uses('Nodes\Geo', 'Common.Nodes');
+use \Nodes\Geo;
 
 /**
  * GeoTest
@@ -71,5 +71,65 @@ class GeoTest extends CakeTestCase {
 		);
 		$return = \Nodes\Geo::getBox('56.123,10.456', '55.123,11.456');
 		$this->assertSame($expected, $return);
+	}
+
+	public function testGetBoundary() {
+		$lat = 0;
+		$lng = 0;
+
+		$radiusKm = 1;
+		$radiusDeg = 360 * $radiusKm / (\Nodes\Geo::EARTH_RADIUS * 2 * M_PI);
+		$diameterDegInnerBox = $radiusDeg * 2;
+
+		$return = \Nodes\Geo::getBoundary($diameterDegInnerBox, $lat, $lng);
+		foreach ($return as &$val) {
+			$val = number_format($val, 6);
+		}
+		unset ($val);
+
+		$expected = array(
+			'lat1' => -0.006382,
+			'lat2' => 0.006382,
+			'lng1' => -0.006382,
+			'lng2' => 0.006382
+		);
+		foreach ($expected as &$val) {
+			$val = number_format($val, 6);
+		}
+		unset ($val);
+
+		$this->assertSame($expected, $return);
+
+		$distances = array(
+			'n' => \Nodes\Geo::getDistance($lat, $lng, $return['lat2'], $lng),
+			'ne' => \Nodes\Geo::getDistance($lat, $lng, $return['lat2'], $return['lng2']),
+			'e' => \Nodes\Geo::getDistance($lat, $lng, $lat, $return['lng2']),
+			'se' => \Nodes\Geo::getDistance($lat, $lng, $return['lat1'], $return['lng2']),
+			's' => \Nodes\Geo::getDistance($lat, $lng, $return['lat1'], $lng),
+			'sw' => \Nodes\Geo::getDistance($lat, $lng, $return['lat1'], $return['lng1']),
+			'w' => \Nodes\Geo::getDistance($lat, $lng, $lat, $return['lng1']),
+			'nw' => \Nodes\Geo::getDistance($lat, $lng, $return['lat2'], $return['lng1'])
+		);
+		foreach ($distances as &$val) {
+			$val = number_format($val, 6);
+		}
+		unset ($val);
+
+		$expected = array (
+			'n' => 0.709846,
+			'ne' => 1.003874,
+			'e' => 0.709846,
+			'se' => 1.003874,
+			's' => 0.709846,
+			'sw' => 1.003874,
+			'w' => 0.709846,
+			'nw' => 1.003874
+		);
+		foreach ($expected as &$val) {
+			$val = number_format($val, 6);
+		}
+		unset ($val);
+
+		$this->assertSame($expected, $distances);
 	}
 }
