92c80d87a6bab5e4bdb8d836514260d6ccea426d
AD7six <andydawson76@gmail.com>
2012-12-18 21:00:53 +0100

    add a test for getting an outer box

--- Test/Case/Lib/Nodes/GeoTest.php
+++ Test/Case/Lib/Nodes/GeoTest.php
@@ -73,6 +73,13 @@ class GeoTest extends CakeTestCase {
 		$this->assertSame($expected, $return);
 	}
 
+/**
+ * testGetBoundary
+ *
+ * Getboundary draws a box where the corners are <x> from the center
+ * where <x> is half the first parameter which defines the diagonal
+ * distance across the resultant box
+ */
 	public function testGetBoundary() {
 		$lat = 0;
 		$lng = 0;
@@ -124,6 +131,73 @@ class GeoTest extends CakeTestCase {
 			'sw' => 1.003874,
 			'w' => 0.709846,
 			'nw' => 1.003874
+		);
+		foreach ($expected as &$val) {
+			$val = number_format($val, 6);
+		}
+		unset ($val);
+
+		$this->assertSame($expected, $distances, "Expected distances, in KM differ");
+	}
+
+/**
+ * testGetBoundaryOuterBox
+ *
+ * Demonstrate how to use getBoundary to define a box which includes all coordinates
+ * that are x km from the central coordinate
+ * @return void
+ */
+	public function testGetBoundaryOuterBox() {
+		$lat = 0;
+		$lng = 0;
+
+		$radiusKm = 1;
+		$radiusDeg = 360 * $radiusKm / (Geo::EARTH_RADIUS * 2 * M_PI);
+		$diameterDegInnerBox = $radiusDeg * 2 * sqrt(2);
+
+		$return = Geo::getBoundary($diameterDegInnerBox, $lat, $lng);
+		foreach ($return as &$val) {
+			$val = number_format($val, 6);
+		}
+		unset ($val);
+
+		$expected = array(
+			'lat1' => -0.009026,
+			'lat2' => 0.009026,
+			'lng1' => -0.009026,
+			'lng2' => 0.009026
+		);
+		foreach ($expected as &$val) {
+			$val = number_format($val, 6);
+		}
+		unset ($val);
+
+		$this->assertSame($expected, $return);
+
+		$distances = array(
+			'n' => Geo::getDistance($lat, $lng, $return['lat2'], $lng),
+			'ne' => Geo::getDistance($lat, $lng, $return['lat2'], $return['lng2']),
+			'e' => Geo::getDistance($lat, $lng, $lat, $return['lng2']),
+			'se' => Geo::getDistance($lat, $lng, $return['lat1'], $return['lng2']),
+			's' => Geo::getDistance($lat, $lng, $return['lat1'], $lng),
+			'sw' => Geo::getDistance($lat, $lng, $return['lat1'], $return['lng1']),
+			'w' => Geo::getDistance($lat, $lng, $lat, $return['lng1']),
+			'nw' => Geo::getDistance($lat, $lng, $return['lat2'], $return['lng1'])
+		);
+		foreach ($distances as &$val) {
+			$val = number_format($val, 6);
+		}
+		unset ($val);
+
+		$expected = array (
+			'n' => 1.003929,
+			'ne' => 1.419769,
+			'e' => 1.003929,
+			'se' => 1.419769,
+			's' => 1.003929,
+			'sw' => 1.419769,
+			'w' => 1.003929,
+			'nw' => 1.419769
 		);
 		foreach ($expected as &$val) {
 			$val = number_format($val, 6);
